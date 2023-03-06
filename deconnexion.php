79b6bb4ad8761ade041e362b1ee1b605e15905e1
Jean <90519788+Jean2B@users.noreply.github.com>
2023-03-06 14:32:24 +0100

    Suppression fichiers inutiles

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset ($_SESSION["mail_user"]);
-echo "<script language='javascript'>";
-echo "window.parent.document.location.href = 'authentification.php';";
-echo "</script>";
-?>
